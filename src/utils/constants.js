2fa678c085ef70f989200a4d6b5b1106b190c608
GitTuanKiet <tuank1792@gmail.com>
2024-01-05 09:02:31 +0700

    push code

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,7 +3,8 @@ export const DOMAINS = [
   'https://trello-web-git-master-vertuankiets-projects-884416e5.vercel.app',
   'https://trello-web-vertuankiets-projects-884416e5.vercel.app',
   'https://trello-gf26y2qqk-vertuankiets-projects-884416e5.vercel.app',
-  'https://trello-web-eight.vercel.app'
+  'https://trello-web-eight.vercel.app',
+  'https://master.d1t3njxi26pe4b.amplifyapp.com'
 ]
 
 export const OBJECT_ID_REGEX = /^[0-9a-fA-F]{24}$/
